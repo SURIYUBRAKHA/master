80ef7d4f06d637aa64e69bd701104701fc52cbcb
SURIYUBRAKHA <116973405+SURIYUBRAKHA@users.noreply.github.com>
2022-10-30 04:09:54 +0700

    Initialize moonshot-bots-with-curve

---
+++ packages/react-app/scripts/s3.js
@@ -0,0 +1,75 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+const s3FolderUpload = require("s3-folder-upload");
+const fs = require("fs");
+
+const directoryName = "build";
+
+const BUCKETNAME = "moonshotbotsmainnet"; // <<---- SET YOUR BUCKET NAME AND CREATE aws.json ** see below vvvvvvvvvv
+
+
+ const invalidation = {
+  awsDistributionId: "E242VNBMA1BDYN",
+  awsInvalidationPath: "/*"
+ }
+
+
+if (!BUCKETNAME) {
+  console.log("☢️   Enter a bucket name in packages/react-app/scripts/s3.js ");
+  process.exit(1);
+}
+
+let credentials = {};
+try {
+  credentials = JSON.parse(fs.readFileSync("aws.json"));
+} catch (e) {
+  console.log(e);
+  console.log(
+    '☢️   Create an aws.json credentials file in packages/react-app/ like { "accessKeyId": "xxx", "secretAccessKey": "xxx", "region": "xxx" } ',
+  );
+  process.exit(1);
+}
+
+credentials.bucket = BUCKETNAME;
+
+// optional options to be passed as parameter to the method
+const options = {
+  useFoldersForFileTypes: false,
+  useIAMRoleCredentials: false,
+};
+
+/// //////////
+/// ////////// First, let's automatically create the bucket if it doesn't exist...
+/// //////////
+
+// eslint-disable-next-line import/no-extraneous-dependencies
+const AWS = require("aws-sdk");
+// Load credentials and set Region from JSON file
+AWS.config.loadFromPath("./aws.json");
+
+// Create S3 service object
+s3 = new AWS.S3({ apiVersion: "2006-03-01" });
+
+// Create params JSON for S3.createBucket
+const bucketParams = {
+  Bucket: BUCKETNAME,
+  ACL: "public-read",
+};
+
+// Create params JSON for S3.setBucketWebsite
+const staticHostParams = {
+  Bucket: BUCKETNAME,
+  WebsiteConfiguration: {
+    ErrorDocument: {
+      Key: "index.html",
+    },
+    IndexDocument: {
+      Suffix: "index.html",
+    },
+  },
+};
+
+
+///
+/// After the bucket is created, we upload to it:
+///
+s3FolderUpload(directoryName, credentials, options , invalidation );
