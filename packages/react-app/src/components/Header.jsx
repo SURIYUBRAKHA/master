1e3166b573208d2552eaea65edc6348dc99ebaf1
SURIYUBRAKHA <116973405+SURIYUBRAKHA@users.noreply.github.com>
2022-10-30 04:10:17 +0700

    Initialize merge-fractal-many-minter

---
+++ packages/react-app/src/components/Header.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { Typography } from "antd";
+
+const { Title, Text } = Typography;
+
+// displays a page header
+
+export default function Header({ link, title, subTitle, ...props }) {
+  return (
+    <div style={{ display: "flex", justifyContent: "space-between", padding: "1.2rem" }}>
+      <div style={{ display: "flex", flexDirection: "column", flex: 1, alignItems: "start" }}>
+        <a href={link} target="_blank" rel="noopener noreferrer">
+          <Title level={4} style={{ margin: "0 0.5rem 0 0" }}>
+            🎉 mint many merge fractals
+          </Title>
+        </a>
+        <Text type="secondary" style={{ textAlign: "left" }}>
+          <a target="_blank" href={"https://opensea.io/collection/mergefractals"} rel="noreferrer">
+            view on opensea
+          </a>
+        </Text>
+      </div>
+      {props.children}
+    </div>
+  );
+}
+
+Header.defaultProps = {
+  link: "https://github.com/austintgriffith/scaffold-eth",
+  title: "🏗 scaffold-eth",
+  subTitle: "forkable Ethereum dev stack focused on fast product iteration",
+};
