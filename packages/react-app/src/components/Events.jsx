6f70112d227b9047ecb492e0138637053f6e9633
SURIYUBRAKHA <116973405+SURIYUBRAKHA@users.noreply.github.com>
2022-10-30 04:09:21 +0700

    Initialize pos-king

---
+++ packages/react-app/src/components/Events.jsx
@@ -0,0 +1,36 @@
+import { List } from "antd";
+import { useEventListener } from "eth-hooks/events/useEventListener";
+import Address from "./Address";
+
+/**
+  ~ What it does? ~
+
+  Displays a lists of events
+
+  ~ How can I use? ~
+
+  <Events
+    contracts={readContracts}
+    contractName="YourContract"
+    eventName="SetPurpose"
+    localProvider={localProvider}
+    mainnetProvider={mainnetProvider}
+    startBlock={1}
+  />
+**/
+
+export default function Events({ contracts, contractName, eventName, localProvider, mainnetProvider, startBlock, renderFunction, title }) {
+  // 📟 Listen for broadcast events
+  const events = useEventListener(contracts, contractName, eventName, localProvider, startBlock);
+
+  return (
+    <div style={{ width: 600, margin: "auto", marginTop: 32, paddingBottom: 32 }}>
+      <h2>{title?title:"Events:"}</h2>
+      <List
+        bordered
+        dataSource={events}
+        renderItem={renderFunction}
+      />
+    </div>
+  );
+}
